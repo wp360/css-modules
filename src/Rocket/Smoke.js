d8d720585e4372c2e4f806c6ce37d09e6769587b
wp360 <2897406527@qq.com>
2017-09-09 21:37:25 +0800

    add file

---
+++ src/Rocket/Smoke.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Smoke.css';
+
+export default class Smoke extends React.Component {
+  render() {
+    return (
+      <div className={styles.smoke}>
+        <video className={styles.video} autoPlay loop src="/smoke.mov" />
+      </div>
+    );
+  }
+}
